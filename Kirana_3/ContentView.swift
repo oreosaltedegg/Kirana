5a39c575395303b428fa54f81047012f2d885f34
Anabel <anabel.oklana03@gmail.com>
2026-09-14 20:49:16 +0800

    Initial Commit

---
+++ Kirana_3/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Kirana_3
+//
+//  Created by Anabel Oklana on 14/09/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Kirana_3/Kirana_3App.swift
@@ -0,0 +1,17 @@
+//
+//  Kirana_3App.swift
+//  Kirana_3
+//
+//  Created by Anabel Oklana on 14/09/26.
+//
+
+import SwiftUI
+
+@main
+struct Kirana_3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
